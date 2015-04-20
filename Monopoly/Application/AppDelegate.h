4dcfbc86a98e7d5abb92626218105f009eaf3c38
Andrew Raukut <raukuts@goozix.com>
2015-04-20 13:37:37 +0300

    [*] Init commit

---
+++ Monopoly/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Monopoly
+//
+//  Created by Andrew Raukut on 20.04.15.
+//  Copyright (c) 2015 raukutsCorporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
